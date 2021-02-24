6baab1e80d96d017bd57e8f1679d4d8745d88fd2
SamedhG <gupta.sam@husky.neu.edu>
2021-02-24 11:25:57 -0500

    Update game logic

--- lib/bulls/game.ex
+++ lib/bulls/game.ex
@@ -1,13 +1,24 @@
 defmodule Bulls.Game do
 
   # Create a new game instance
-  def new() do
+  # A Guess contains a n-tuple of n players
+  # Players is a tuple of player names
+  def new(players) do
     %{
       secret: random_secret(),
-      guesses: []
+      guesses: [],
+      players: players,
     }
   end
 
+  # Assumes that all guesses have been validated previously
+  def guess(st, guesses) do
+    if length(st.players) == length(guesses) do
+      { :ok, %{ st | guesses: [ guesses | st.guesses ] } }
+    else
+      { :error, "Invalid number of guesses" }
+    end
+  end
 
   # Checks for the following things:
   # - Repeats in the guess
@@ -15,40 +26,40 @@ defmodule Bulls.Game do
   # - Repeats with other guesses
   # - The length should be 4
   # returns a new state if the guess was valid else returns an error object
-  def guess(st, guess) do 
+  def validate(st, guess) do 
     cond do
-      Enum.member?(st.guesses, st.secret) -> 
-        { :error, "You Win" }
-      length(st.guesses) >= 8 -> 
-        { :error, "You Lose" }
       String.length(guess) != 4 -> 
         { :error, "The length is not 4" }
       guess |> String.graphemes |> Enum.any?(&(&1 < "0" || &1 > "9")) -> 
         { :error, "All chars need to be digits" }
-      Enum.member?(st.guesses, guess) -> 
+      st.guesses |> List.flatten |> Enum.member?(guess) -> 
         { :error, "Repeated guess" }
       guess |> String.graphemes |> Enum.uniq |> length != 4 ->
         { :error, "All chars in the guess should be unique" }
-      true -> {:ok, %{ st | guesses: [ guess | st.guesses ] } }
+      true -> :ok
     end
   end
 
 
   # Get the current view in the following format:
   # If the game is over then the returned object is :
-  # { game_over: true, message: "reason" }
+  # { game_over: true, results: %{"p1" => "win", "p2" => "lose" ...} }
   # If the game is still going then the returned object is 
   # {game_over: false, guesses: [table of guess and scores]}
   def view(st) do
     cond do
-      Enum.member?(st.guesses, st.secret) -> 
-        %{ game_over: true, message: "You Win" }
-      length(st.guesses) >= 8 -> 
-        %{ game_over: true, message: "You Lose"}
+      Enum.member?(hd(st.guesses), st.secret) ->
+        results = Map.new(Enum.zip(st.players, Enum.map(hd(st.guesses), &(if &1 === st.secret do "win" else "lose" end))))
+        %{ game_over: true, results: results}
       true ->
+        guesses = Enum.map(st.guesses, 
+          fn y -> 
+            Enum.map(y, &(view_1(&1, st.secret))) 
+        end)
+        guesses = Enum.map(guesses, &(st.players |> Enum.zip(&1) |> Map.new))
         %{ 
           game_over: false, 
-          guesses: st.guesses |> Enum.map(&(view_1(&1, st.secret))) 
+          guesses: guesses 
         }
     end
 
